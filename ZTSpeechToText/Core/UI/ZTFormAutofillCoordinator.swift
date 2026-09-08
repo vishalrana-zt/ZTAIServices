a2b98398177ab68e508ad40b979df011ea41ef7a
vishalrana-zt <vishal.rana@zentrades.pro>
2026-09-08 19:04:19 +0530

    update

--- ZTSpeechToText/Core/UI/ZTFormAutofillCoordinator.swift
+++ ZTSpeechToText/Core/UI/ZTFormAutofillCoordinator.swift
@@ -123,7 +123,7 @@ public final class ZTFormAutofillCoordinator: ObservableObject {
                 return
             }
             do {
-                try self.speechBridge.start(configuration: .init(mode: .postRecording))
+                try await self.speechBridge.start(configuration: .init(mode: .postRecording))
             } catch {
                 step = .error(error.localizedDescription)
             }
