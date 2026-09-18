6ad4c677faefaea67dbacaff52f31cd68a971f25
vishalrana-zt <vishal.rana@zentrades.pro>
2026-09-18 13:30:25 +0530

    update

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "ZTAIServices",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v16)
+        .iOS(.v18)
     ],
     products: [
         .library(
